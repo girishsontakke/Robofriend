24348032c2b4e3679b89f4cdf0a90266a499ac86
girishsontakke <girishsontakke7@gmail.com>
2020-07-30 10:38:43 +0530

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
