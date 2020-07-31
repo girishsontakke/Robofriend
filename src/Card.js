c916a8593e9ae5e92669d40450685a8ca43148ee
girishsontakke <girishsontakke7@gmail.com>
2020-07-31 15:56:46 +0530

    make text centre

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({id, name, email}) => {
     return (
-        <div className='bg-light-green dib br3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-light-green dib br3 ma2 grow bw2 shadow-5'>
             <img alt="robots" src='https://robohash.org/test'/>
             <div>
                 <h2> {name} </h2>
