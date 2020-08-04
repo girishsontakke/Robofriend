e3633009487b60eecffc0ff5dbc0f4d498619e9a
girishsontakke <girishsontakke7@gmail.com>
2020-08-04 19:22:36 +0530

    remove cardlist and use app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
+import App from "./App"
 import 'tachyons';
-import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots = { robots }/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
