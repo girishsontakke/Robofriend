1cb713bed8948ee9698a9ff392ec89c41bbe45f7
girishsontakke <girishsontakke7@gmail.com>
2020-09-08 12:55:47 +0530

    replace robots with user

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -6,12 +6,7 @@ const CardList = ({ robots }) => {
     <div style={{ margin: "0 auto", width: "80%" }}>
       {robots.map((user, i) => {
         return (
-          <Card
-            key={i}
-            id={robots[i].id}
-            name={robots[i].name}
-            email={robots[i].email}
-          />
+          <Card key={i} id={user.id} name={user.name} email={user.email} />
         );
       })}
     </div>
