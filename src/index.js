bc2dc0ebab548aed8b452f9d7a09ac2533cfad72
girishsontakke <girishsontakke7@gmail.com>
2020-09-12 11:19:46 +0530

    pass store of redux to app

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import "./index.css";
 import App from "./containers/App";
 import "tachyons";
 
+//redux
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { searchRobots } from "./reducers";
+
+const store = createStore(searchRobots);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
