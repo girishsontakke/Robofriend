f6ca4f8e6b559a0516e245fd1ae033bd757bd7ba
girishsontakke <girishsontakke7@gmail.com>
2020-09-17 12:59:44 +0530

    action for fetching robots

--- src/action.js
+++ src/action.js
@@ -1,5 +1,19 @@
-import { CHANGE_SEARCH_FIELD } from "./variable";
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from "./variable";
+
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text,
 });
+
+export const requestRobots = () => (dispatch) => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING });
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((responses) => responses.json())
+    .then((data) => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+    .then((error) => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }));
+};
