3b0cf858030f614db4d7763135345b7b520987cc
girishsontakke <girishsontakke7@gmail.com>
2020-12-31 21:38:37 +0530

    convert class based component to functional component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import SearcBox from "../components/SearchBox";
 import Cardlist from "../components/CardList";
 import Scroll from "../components/Scroll";
@@ -17,29 +17,35 @@ const mapDispatchToProps = (dispatch) => ({
   onRequestRobots: () => dispatch(requestRobots()),
 });
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-  render() {
-    const { onSearchChange, searchField, robots, isPending } = this.props;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-    return isPending ? (
-      <h1 className="tc"> loading </h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1"> RoboFriends </h1>
-        <SearcBox searchChange={onSearchChange} />
-        <Scroll>
-          <ErrorBoundry>
-            <Cardlist robots={filteredRobots} />
-          </ErrorBoundry>
-        </Scroll>
-      </div>
-    );
-  }
-}
+const App = (props) => {
+  const {
+    onRequestRobots,
+    onSearchChange,
+    searchField,
+    robots,
+    isPending,
+  } = props;
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  useEffect(() => {
+    onRequestRobots();
+  }, []);
+
+  return isPending ? (
+    <h1 className="tc"> loading </h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1"> RoboFriends </h1>
+      <SearcBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <Cardlist robots={filteredRobots} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
