280e92a1972a1b5763c8b976202180ba72678091
girishsontakke <girishsontakke7@gmail.com>
2020-12-31 22:10:16 +0530

    add margin bottom to component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const SearcBox = (props) =>{
-    return(
-    <input 
-    onChange = {props.searchChange}
-    className = 'pa3 ba b--green bg-lightest-blue'
-    type='search' 
-    placeholder='search robots'
+const SearcBox = (props) => {
+  return (
+    <input
+      onChange={props.searchChange}
+      className="pa3 ba b--green bg-lightest-blue mb3"
+      type="search"
+      placeholder="search robots"
     />
-    );
-}
+  );
+};
 
 export default SearcBox;
