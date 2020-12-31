6b19d84dc7f03dbc7071772993c80795af0cee38
girishsontakke <girishsontakke7@gmail.com>
2020-12-31 21:37:49 +0530

    cursor: pointer

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({id, name, email}) => {
-    return (
-        <div className='tc bg-light-green dib br3 ma2 grow bw2 shadow-5'>
-            <img alt="robots" src={`https://robohash.org/${id}?200x200`}/>
-            <div>
-                <h2> {name} </h2>
-                <p>{email}</p>
-            </div>            
-        </div>
-    )
-}
+const Card = ({ id, name, email }) => {
+  return (
+    <div
+      className="tc bg-light-green dib br3 ma2 grow bw2 shadow-5 "
+      style={{ cursor: "pointer" }}
+    >
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h2> {name} </h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
